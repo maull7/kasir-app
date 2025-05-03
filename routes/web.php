d2706f545c26b478a58dd07b2a1e754475ecf1df
vindy-ardiana <vindyardianaputri19@gmail.com>
2025-05-03 12:40:43 +0700

    abis bikin form login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
+// 🛡️ Proteksi route '/' pakai middleware auth
 Route::get('/', function () {
     return view('index');
-});
+})->middleware('auth');
+
+// 🛠️ Route login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
